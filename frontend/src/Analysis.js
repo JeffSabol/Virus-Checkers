2221f95705c99dc1a69295f2b4b57844949aa0c3
Nathan Tamiru <83526475+NathanTamiru@users.noreply.github.com>
2022-04-17 21:50:48 -0400

    Update Analysis.js

--- frontend/src/Analysis.js
+++ frontend/src/Analysis.js
@@ -9,15 +9,26 @@ const baseUrl = "http://localhost:5000";
 const Analysis = () =>
   {
     const [Hash, setHash] = useState(""); 
+    const [virusReply, setVirusReply] = useState(0); 
+    const [curdsList, setcurdsList] = useState([0]);
+    const [isLoading, setIsLoading] = useState([0]);
 
 
     const handleChangeHash = e => {
+      
       setHash(e.target.value);
       console.log(Hash)
   }
-
+  useEffect(() => {
+    
+  }, []);
+  
+ 
+  
   const handleSubmit = async (e) => {
+    
     e.preventDefault();
+    setVirusReply(1);
     console.log("called here")
     
     /*Normal Update */
@@ -25,11 +36,13 @@ const Analysis = () =>
         try {
             const data = await axios.post(`${baseUrl}/hashes`, {
                 Hash: Hash
-         
+              
             });
+            
+            setcurdsList([data.data]);
             /** Update dataset list entries**/
             /** Reset entries**/
-            setHash('');
+            setHash("");
             
         } catch (err) {
             console.log(err.message);
@@ -40,28 +53,98 @@ const Analysis = () =>
   
   
   return (
-
     
    
          <div className="App-header">
-          <section>
-          <h1 id="title" class="hidden"><span id="logo">Virus <span>Checkers</span></span></h1>
 
-          </section>
-          
-	<div class="container">
-    <form  onSubmit={handleSubmit} >
+{ curdsList.map(curd=> {if(virusReply === 0) {
+ return(<td>          <section>
+  
+  
+  
+  <div id="VirusCheckers">
+    <img id="VirusLogo" src="https://i.imgur.com/IyLfb58.png"></img>      
+    <h1 id="title" class="hidden"><span id="logo">Virus <span>Checkers</span></span></h1>
+  </div>
 
 
+  
+  </section>
+  
+<div class="container">
+<form  onSubmit={handleSubmit} >
+<input  onChange = {handleChangeHash} class="search" type="text" id="search" />
+<input  class="submit" type="submit" value="Check Hash " />
+</form>
+
+</div></td>)
+
+}
+else
+{
+  
+  console.log({curd});
+  try {
+    if(curd.error.code)
+  {
+    return(   
+      <h1>   
+       This doesn't appear to be a virus.
+      </h1>
+      )
+  }
+    
+  } 
+  catch (error) {
+    while(isLoading)
+    {
+      console.log("doing this");
+      
+      {setTimeout(() => {
+        console.log("delayed doing this");
+        setIsLoading();
+      }, 32000)
+    }
+      return(
+
+        <div className="spinner-container">
+      
+        <div className="loading-spinner"></div>
+      </div>
+
+      )
 
 
-    <input  onChange = {handleChangeHash} class="search" type="text" id="search" />
-		<input  class="submit" type="submit" value="Check Hash " />
-    </form>
-		
-	</div>
-         
+    }
+    
+
+      console.log(error);
+      return(   
+  
         
+        <section> 
+  
+  
+ 
+  
+        <h1>{curd["data.attributes.last_analysis_stats.malicious"]} engines find this to be malicious </h1>
+        <tr>Name: {curd["data.attributes.meaningful_name"]}</tr>  
+         <tr> File type: {curd["data.attributes.type_description"]}</tr> 
+          <tr>Threat classification: {curd["data.attributes.popular_threat_classification.suggested_threat_label"]}</tr>  
+
+            </section>
+        
+        )
+
+    
+   
+    
+  }
+
+}
+
+
+})}
 
       </div>
    
@@ -70,4 +153,3 @@ const Analysis = () =>
 
 
 export default Analysis;
-
