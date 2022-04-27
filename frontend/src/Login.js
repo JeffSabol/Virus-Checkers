5be35db3e7568965381a2ebc5b81df97fdc14ebd
Nathan Tamiru <83526475+NathanTamiru@users.noreply.github.com>
2022-04-27 19:14:34 -0400

    Update Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -3,14 +3,55 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { BrowserRouter } from 'react-router-dom';
 import Analysis from "./Analysis.js";
+import axios from "axios"
 
-
+const baseUrl = "http://localhost:5000";
 const Login = () => {
 
-    const handleLogin = () => {
 
-    }
+	const [Username, setUsername] = useState(""); 
+	const [Password, setPassword] = useState(""); 
+
+	const [credsList, setCredsList] = useState([0]);
+    const [signup, setSignUp] = useState([0]);
+
+
+	const handleChangeUsername = e => {
+      
+		setUsername(e.target.value);
+		
+	}
+	const handleChangePassword = e => {
+      
+		setPassword(e.target.value);
+		
+	}
+
+	const handleLogin = async (e) => {
     
+		e.preventDefault();
+		console.log("logining in")
+		
+		/*Normal Update */
+		
+			try {
+				const data = await axios.post(`${baseUrl}/login`, {
+					Username: Username,
+					PasswordHash:Password
+				  
+				});
+				
+				setCredsList([data.data]);
+				/** Update dataset list entries**/
+				/** Reset entries**/
+				setUsername("");
+				setPassword("");
+				
+			} catch (err) {
+				console.log(err.message);
+			}
+		
+	}
     
   
 
@@ -24,19 +65,22 @@ const Login = () => {
 		<div class="top">
 			<h1 id="title" class="hidden"><span id="logo">Virus <span>Checkers</span></span></h1>
 		</div>
+		
 		<div class="login-box animated fadeInUp">
 			<div class="box-header">
 				<h2>Log In</h2>
 			</div>
 			<label for="username">Username</label>
 			<br/>
-			<input type="text" id="username" />
+			<input onChange = {handleChangeUsername} type="text" id="username" />
 			<br/>
 			<label for="password">Password</label>
 			<br/>
-			<input type="password" id="password" />
+			<input  onChange = {handleChangePassword} type="password" id="password" />
 			<br/>
-			<button type="submit">Sign In</button>
+			<form  onSubmit={handleLogin} ><button type="submit">Sign In</button></form>
+			
+
 			<br/>
 			<span style={{ color: "#a29494", textAlign: "center", display: "inline-block", width: "100%" }}>
             Don't have a account? <Link to="/signup">Create account</Link>
