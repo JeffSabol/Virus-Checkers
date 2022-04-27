4b151fa824f3576f24aabeaf14295f29a1c3efa5
Nathan Tamiru <83526475+NathanTamiru@users.noreply.github.com>
2022-04-27 19:12:57 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,13 +8,15 @@ const defaultTimedInstance = nvt.makeAPI();
 const flatten = require('flat').flatten;
 
 let hashExists;
+let usernameExists;
+let emailExists;
 
 let app= express();
 
 app.use(cors());
 app.use(bodyparser.json());
 
-var mySQLconnection = mysql.createConnection({
+var mySQLconnection = mysql.createPool({
 host: 'localhost',
 user:'root',
 password:'password',
@@ -25,35 +27,60 @@ multipleStatements: true
 
 });
 
-mySQLconnection.connect((err)=> {
-if(!err){
-console.log("CONNECTED\n\n\n");
-}
-else{
-
-    console.log(JSON.stringify(err, undefined, 2));
-}
-
-
-
+var userCredDatabase = mysql.createPool({
+    host: 'localhost',
+    user:'root',
+    password:'password',
+    database: '447UserCred',
+    multipleStatements: true
+    
+    /**use ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';**/
+    
 });
+
+
 
 
 
 app.listen(5000, ()=>console.log("Express server is running at port no :5000"));
 
 
-function entryExists(checkHash, paramsId) {
+  function hashEntryExists (checkHash, paramsId) {
 
     var sql = "SELECT * FROM HashId WHERE EXISTS (SELECT Hash FROM HashId WHERE Hash = '"+checkHash+"');";
 
-    mySQLconnection.query(sql, paramsId, (err,rows,fields)=>{
+      mySQLconnection.query(sql, paramsId, (err,rows,fields)=>{
         if(!rows[0]) { hashExists = false; }
         else{hashExists = true; }
 
      }
     );
     
+}
+  function usernameEntryExists(checkUsername, paramsId) {
+
+    var sql = "SELECT * FROM userCredTable WHERE EXISTS (SELECT Username FROM userCredTable WHERE Username = '"+checkUsername+"');";
+
+      userCredDatabase.query(sql, paramsId, (err,rows,fields)=>{
+        if(!rows[0]) { usernameExists = false; }
+        else{usernameExists = true; }
+      
+     }
+    );
+    
+}
+
+  function emailEntryExists(checkEmail, paramsId) {
+
+    var sql = "SELECT * FROM userCredTable WHERE EXISTS (SELECT Email FROM userCredTable WHERE Email = '"+checkEmail+"');";
+
+     userCredDatabase.query(sql, paramsId, (err,rows,fields)=>{
+        if(!rows[0]) { emailExists = false; }
+        else{emailExists = true; }
+
+     }
+    );
+
 }
 
 
@@ -93,9 +120,71 @@ app.get('/hashes/:id', (req, res)=>{
 
 app.post('/hashes', (req, res)=> {
 
-        entryExists(req.body.Hash, [req.params.id]);
+        hashEntryExists(req.body.Hash, [req.params.id]);
         
         //wait for sql reply and edit
+        setTimeout(() => {
+            if(!hashExists)
+                {
+        
+                    console.log("Hash doesn't exist in our database... checking external databases");
+        
+                    //API virus total
+                    //7a1937dfdad30b004dae4dd55fd49d28efa658d464dab3df61b5c91b15934eea         
+                    //change default key in v3
+                    const theSameObject = defaultTimedInstance.fileLookup(req.body.Hash, function(err, resp){
+                        if (err) {
+                          console.log('Well, crap.');
+                         res.send(err);
+                          return;
+                        }
+    
+                        
+                        var sql = "INSERT INTO `HashId` (`Hash`) VALUES ('" + req.body.Hash +  "')";
+                    mySQLconnection.query(sql,  [req.params.id],(err,rows,fields)=>{
+                        if(!err){
+                        
+                            console.log("VIRUS HASH FOUND- ADDED TO DATABASE");                    
+                        }
+                        else{
+                        
+                            console.log(err);
+                        }
+                 }
+                        )
+    
+                       res.send(flatten(JSON.parse(resp)));
+                        return resp;
+                      });
+        
+                }
+                else
+                {
+        
+                    console.log("THIS EXISTS IN OUR DATABASE- NOW DISPLAYING METADATA");
+    
+                    const theSameObject = defaultTimedInstance.fileLookup(req.body.Hash, function(err, resp){
+                        if (err) {
+                          console.log('Well, crap.');
+                         res.send(err);
+                          return;
+                        }
+                        
+    
+                       res.send(flatten(JSON.parse(resp)));
+                        return resp;
+                      });  
+                }
+        }, 1000);
+      
+
+        });
+
+        app.post('/hashes', (req, res)=> {
+
+        hashEntryExists(req.body.Hash, [req.params.id]);
+        
+        //wait for sql reply sand edit
         setTimeout(() => {
             if(!hashExists)
                 {
@@ -134,6 +223,7 @@ app.post('/hashes', (req, res)=> {
                 }
                 else
                 {
+                    
         
                     console.log("THIS EXISTS IN OUR DATABASE- NOW DISPLAYING METADATA");
     
@@ -154,4 +244,73 @@ app.post('/hashes', (req, res)=> {
 
         });
 
-     
+
+
+
+        app.post('/login', (req, res)=> {
+
+           console.log(req.body.Username+" tried to login")
+          
+    
+            });   
+
+
+
+            app.post('/signup', (req, res)=> {
+
+           
+                console.log(req.body.FullName+" tried to SignUp")
+
+               usernameEntryExists(req.body.Username,[req.params.id])
+                emailEntryExists(req.body.Email,[req.params.id])
+
+                setTimeout(() => {
+                    console.log("Emailexists: "+emailExists)
+                    console.log("Unameexists: "+usernameExists) 
+
+                    if(usernameExists && emailExists){
+
+                        return res.send({hiddenU:"Username",hiddenE:"Email"});
+
+                    }
+
+                    else if(usernameExists){
+
+
+                        return res.send({hiddenU:"Username",hiddenE:""});
+                    }
+                   else if(emailExists){
+
+
+                        return res.send({hiddenU:"",hiddenE:"Email"});
+                    }
+                    //PASSOWRD ENCRYPTION HERE
+
+                    var sql ="INSERT INTO `userCredTable` (`FullName`, `Email`,`Username`,`PasswordHash`) VALUES ('" + req.body.FullName +  "','" + req.body.Email  +  "','" + req.body.Username  +  "','" + req.body.PasswordHash  +  "')";
+                    userCredDatabase.query(sql,  [req.params.id],(err,rows,fields)=>{
+                       if(!err){
+                            
+                           console.log("New user created "); 
+                           return res.send({status:"success"});                   
+                       }
+                       else{
+                       
+                           console.log(err);
+                           return res.send({status:"failure"}); 
+                       }
+                }
+                       )
+                }, 1000);
+                
+
+
+                
+                   
+                    
+            
+               
+
+            
+    
+            });  
+
