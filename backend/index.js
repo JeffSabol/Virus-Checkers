62c796ec7b93ae1dc95caed6904603698e2ac9ea
NathanTamiru <mr53017@umbc.edu>
2022-05-03 23:20:16 -0400

    User and Hash Database: update details if Hash already exists

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,11 @@ let app= express();
 app.use(cors());
 app.use(bodyparser.json());
 
+
+
+
+
+
 var mySQLconnection = mysql.createPool({
 host: 'localhost',
 user:'root',
@@ -402,3 +407,37 @@ app.post('/hashes', (req, res)=> {
     
             });  
 
+
+            /**CREATE SCHEMA sql447;
+            CREATE TABLE sql447.HashId(
+            Id INT  Primary key,
+            Hash Varchar(255),
+            PopularName Varchar(255),
+            FileType varchar(255),
+            MD5rep   varchar(255),
+            Sha256rep   varchar(255),
+            Threat Varchar(255),
+            FileSize INT,
+            FirstSub INT,
+            LastSub INT,
+            NumTimeSub   INT
+            
+            
+            );
+            
+            CREATE SCHEMA 447UserCred;
+            CREATE TABLE 447UserCred.userCredTable(
+            PersonId INT  Primary key,
+            FullName Varchar(255),
+            Email varchar(255),
+            Username   varchar(255),
+            PasswordHash   varchar(255)
+            
+            
+            );
+            
+            
+            
+            
+            **/
+            
